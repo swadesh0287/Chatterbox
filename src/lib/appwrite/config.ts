5ff243e627ae5334a835a782733ea470e4db94d0
swadesh0287 <76160679+swadesh0287@users.noreply.github.com>
2024-01-04 00:05:03 +0530

    first commit

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,27 @@
+import { Client, Account, Databases, Storage, Avatars } from "appwrite";
+
+// const client = new Client();
+
+// client
+//     .setEndpoint('https://cloud.appwrite.io/v1')
+//     .setProject('65870bc54cb2a124f5bf');
+
+export const appwriteConfig = {
+  url: import.meta.env.VITE_APPWRITE_URL,
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+  postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+  savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
+};
+
+export const client = new Client();
+
+client.setEndpoint(appwriteConfig.url);
+client.setProject(appwriteConfig.projectId);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const avatars = new Avatars(client);
